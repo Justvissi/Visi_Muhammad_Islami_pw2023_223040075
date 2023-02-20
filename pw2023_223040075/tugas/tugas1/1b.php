3eb2b51a4a3d3f30168819320329a070ee9b4980
visi muhammad islami <121602442+Justvissi@users.noreply.github.com>
2023-02-20 15:15:56 +0700

    Add files via upload

---
+++ pw2023_223040075/tugas/tugas1/1b.php
@@ -0,0 +1,25 @@
+<?php  
+$nrp = 75;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        Aku adalah angka<b><?php echo " $nrp" ?></b>
+    </p>
+    <p>Jika aku dikali 5, maka aku sekarang menjadi <b><?php echo $nrp*5 ?></b>
+</p>
+    <p>Jika aku dibagi 2, maka aku sekarang menjadi <b><?php echo $nrp*5/2 ?></b>
+</p>
+    <p>Jika aku di tambah 75, maka aku sekarang menjadi <b><?php echo $nrp*5/2+75 ?></b>
+</p>
+    <p>Jika aku dikurang 20, maka aku sekarang menjadi <b><?php echo $nrp*5/2+75-25 ?></b>
+</p>
+</body>
+</html>
---
+++ pw2023_223040075/tugas/tugas1/1c.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="b1">
+    <div><p>1</p></div>
+    </div>
+    <div class="b2">
+        <div><p>2</p></div>
+        <div><p>2</p></div>
+    </div>
+    <div class="b3">
+        <div><p>3</p></div>
+        <div><p>3</p></div>
+        <div><p>3</p></div>
+    </div>
+</body>
+</html>
